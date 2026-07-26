f44365f66ab64022fc61f9956f3dfe487ad17632
PoSayDone <29358657+PoSayDone@users.noreply.github.com>
2026-07-26 19:40:42 +0000

    chore: bump version to 0.6 (6) [skip ci]

--- app/mobile/build.gradle.kts
+++ app/mobile/build.gradle.kts
@@ -69,8 +69,8 @@ android {
         applicationId = "io.github.posaydone.kinopub.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
