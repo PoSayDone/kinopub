c50b5bd123922aa15885562943650fa4338d343d
PoSayDone <29358657+PoSayDone@users.noreply.github.com>
2026-07-26 19:48:26 +0000

    chore: bump version to 0.7 (7) [skip ci]

--- app/tv/build.gradle.kts
+++ app/tv/build.gradle.kts
@@ -69,8 +69,8 @@ android {
         applicationId = "io.github.posaydone.kinopub.tv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
         vectorDrawables {
             useSupportLibrary = true
         }
